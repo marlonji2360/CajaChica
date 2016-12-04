9109a2d659cb73d781cdfff84bf97265a620e3c4
marlonji2360 <marlonji2360@gmail.com>
2016-12-04 10:53:03 -0600

    Inciamos Proyecto Caja Chica

---
+++ ProyectoCajaChica/ProyectoCajaChica/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoCajaChica.Startup))]
+namespace ProyectoCajaChica
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
